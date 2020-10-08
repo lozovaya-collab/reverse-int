485e4aab944a4906eb6023aa06202d08ad83cdec
Anya Lozovaya <anyalozovaya2014@gmail.com>
2020-10-09 02:59:18 +0400

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let N = Math.abs(n)
+    let arrayN = String(N).split('').reverse().join('')
+    return Number(arrayN)
 }
